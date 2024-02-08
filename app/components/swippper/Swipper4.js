79ed741291f44fefbc32f57554e0c0baadab3b72
soleil00 <srukundo01@gmail.com>
2024-02-08 10:21:38 +0200

    reverted change

--- app/components/swippper/Swipper4.js
+++ app/components/swippper/Swipper4.js
@@ -4,24 +4,18 @@ import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Stack } from '@mui/material';
 import 'leaflet/dist/leaflet.css';
-// import L from "leaflet"
-
-// const markerIcon = new L.Icon({
-//       iconUrl: '/icons/user.webp',
-//       iconSize: [35, 50],
-//     });
-
-//     const markerIcon2 = new L.Icon({
-//       iconUrl: '/icons/wintana.webp',
-//       iconSize: [35, 50],
-//     });
 
 const Swipper4 = () => {
   const [coords, setCoords] = useState([]);
 
+  const iconImage = {
+  iconUrl: '/marker.svg',
+    iconSize: [25, 41],
+}
 
- 
+
   useEffect(() => {
+    // Function to fetch user's location
     const getMyLocation = () => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
@@ -38,47 +32,41 @@ const Swipper4 = () => {
       }
     };
 
+    // Call the function to get user's location
     getMyLocation();
   }, []);
 
-
-
- 
-
   return (
     <Stack height="400px" position="relative" zIndex={1000} marginTop="30px">
-     
+      {/* Render the MapContainer only on the client-side */}
+      {typeof window !== 'undefined' && (
         <MapContainer
           center={coords && coords.length ? coords : [-1.932432, 30.1362107]}
           zoom={13}
           style={{ width: '100%', height: '400px' }}
           scrollWheelZoom={true}
         >
+          {/* TileLayer for the map */}
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+
+          {/* Marker with Popup */}
           {coords.length > 0 && (
             <>
-              {L && (
-                <>
-                  <Marker position={coords}
-                    // icon={markerIcon}
-                  >
-                    <Popup>You&apos;re here</Popup>
-                  </Marker>
-                  <Marker position={[-1.9532, 30.1156]}
-                    // icon={markerIcon2}
-                  >
-                    <Popup>Wintana is Here</Popup>
-                  </Marker>
-                </>
-              )}
+              <Marker position={coords}>
+                <Popup>You&apos;re here</Popup>
+              </Marker>
+              <Marker position={[-1.9532, 30.1156]}>
+                <Popup>Wintana is Here</Popup>
+              </Marker>
             </>
           )}
         </MapContainer>
+      )}
     </Stack>
-  )
+  );
 };
 
 export default Swipper4;
