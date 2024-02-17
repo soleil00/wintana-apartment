4b76bb1e805d85430e3da897ee57837ef6d0c761
soleil00 <srukundo01@gmail.com>
2024-02-17 20:55:00 +0200

    fixed contactComp again

--- app/floors/[title]/page.jsx
+++ app/floors/[title]/page.jsx
@@ -4,7 +4,8 @@ import AccomodationRoomCard from "@/app/components/AccomodationRoomCard";
 import BottomControll from "@/app/components/BottomControll";
 import ContactComp from "@/app/components/ContactComp";
 import FooterComp from "@/app/components/FooterComp";
-import { Container, Grid, Stack, Typography } from "@mui/material";
+import { Email, Facebook, Instagram, LocationOn, Twitter, WhatsApp } from "@mui/icons-material";
+import { Button, Container, Grid, Stack, TextField, Typography } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -135,43 +136,120 @@ const page = ({ params }) => {
             kitchen={"1 kitchens"}
             type={"residential"}
           />
-          {/* <AccomodationRoomCard
-          path={"/room3.jpeg"}
-          room={4}
-          display={{ xs: 6, sm: 3, md: 2 }}
-          title={"Room 2"}
-          type={"commercial"}
-          beds={2}
-          link={"/"}
-          toilet={1}
-          kitchen={1}
-          // rooms={2}
-          type={"residential"}
-        />
-        <AccomodationRoomCard
-          path={"/living.webp"}
-          room={4}
-          display={{ xs: 6, sm: 3, md: 2 }}
-          title={"Room 3"}
-          type={"commercial"}
-          beds={3}
-          link={"/"}
-          toilet={1}
-          kitchen={1}
-          // rooms={3}
-          type={"residential"}
-        /> */}
+         
         </Grid>
       </Container>
       
-      <Stack display={{xs:"block",md:"none"}}>
-        <BottomControll />
-        <BottomControll />
-        <BottomControll />
-        <BottomControll />
-        {/* <BottomControll /> */}
-      </Stack>
-      <ContactComp visible={true} />
+     
+       <div className="bg-gray-400 w-[100%] testing-mg " id="contact">
+      <Container className="max-w-[1224px] w-full mx-auto py-4 ">
+        <Stack
+          direction={{ xs: "column", sm: "column", md: "row" }}
+          justifyContent={{ xs: "none", sm: "none", md: "space-between" }}
+          alignItems={"center"}
+          width={"100%"}
+        >
+          
+            <Stack
+              className=" border-gray-400 border rounded-md p-4"
+              width={{ xs: "100%", sm: "100%", md: "30%" }}
+            >
+              <Typography
+                variant="h4"
+                component="div"
+                sx={{ paddingBottom: { xs: "10px", sm: "15px", md: "30px" } }}
+              >
+                Wintana Apartment
+              </Typography>
+              <Typography component="body2" className="mb-[15px] text-[15px]">
+                Wintana Apartments blends commercial vibrancy with residential
+                elegance, offering prime retail spaces on the ground floor and
+                modern accommodations above.
+              </Typography>
+              <Typography component="body2" className="text-[15px]">
+                The fourth floor boasts exclusive pentRooms, while the top floor
+                features a comprehensive fitness and leisure center, including a
+                sauna, pool, fully equipped kitchen, and a stylish bar.
+              </Typography>
+            </Stack>
+      
+
+          {/* <div id="contact" className="w-full sm:w-full md:w-1/3"> */}
+          <Stack
+            className=" border-gray-400 border rounded-md p-4"
+            width={{ xs: "100%", sm: "100%", md: "30%" }}
+            id="contact"
+          >
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{ paddingBottom: { xs: "10px", sm: "15px", md: "30px" } }}
+            >
+              Contact Us
+            </Typography>
+            <Stack justifyContent={"space-between"}>
+              <Stack direction={"row"} spacing={2} alignContent={"center"}>
+                <LocationOn color="secondary" fontSize="large" />
+                <Typography>Kg 452 st</Typography>
+              </Stack>
+              <Stack direction={"row"} spacing={2} alignContent={"center"}>
+                <Instagram color="secondary" fontSize="large" />
+                <Typography>@wintan-apartment</Typography>
+              </Stack>
+              <Stack direction={"row"} spacing={2} alignContent={"center"}>
+                <Facebook color="secondary" fontSize="large" />
+                <Typography>wintana</Typography>
+              </Stack>
+              <Stack direction={"row"} spacing={2} alignContent={"center"}>
+                <WhatsApp color="secondary" fontSize="large" />
+               <a href="tel:0785612510">
+  <Typography>0785612510</Typography>
+</a>
+              </Stack>
+              <Stack direction={"row"} spacing={2} alignContent={"center"}>
+                <Twitter color="secondary" fontSize="large" />
+                <Typography>@Wintana23</Typography>
+              </Stack>
+              <Stack direction={"row"} spacing={2} alignContent={"center"}>
+                <Email color="secondary" fontSize="large" />
+                <Typography>sbhatuare@yahoo.com</Typography>
+              </Stack>
+            </Stack>
+          </Stack>
+          {/* </div> */}
+          
+            <Stack
+              className=" border-gray-400 border rounded-md p-4"
+              width={{ xs: "100%", sm: "100%", md: "30%" }}
+              // height={"370px"}
+              spacing={2}
+            >
+              <TextField
+                fullWidth
+                label="Enter Your Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                fullWidth
+                multiline
+                rows={4}
+                label="Edit here to add Your message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <Button
+                color="secondary"
+                variant="outlined"
+                onClick={handleSendMessage}
+              >
+                Send Message
+              </Button>
+            </Stack>
+          
+        </Stack>
+      </Container>
+    </div>
     </div>
   );
 };
